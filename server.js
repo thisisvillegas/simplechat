ca50cd1b89de5e64182cb6710e45acf624e9e858
Andres <thisisvillegas@gmail.com>
2023-11-17 18:04:29 -0600

    re-organizing

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
